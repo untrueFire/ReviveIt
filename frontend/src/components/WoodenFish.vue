02af2190bd24ead96011f3b3aed9651ebf36251f
testuser4 <admin.copilot991@passinbox.com>
2024-10-21 19:53:29 +0800

    Add animation for wooden fish

--- frontend/src/components/WoodenFish.vue
+++ frontend/src/components/WoodenFish.vue
@@ -1,19 +1,25 @@
 <template>
-	<img width="100" src="/muyu.svg" alt="我是木鱼" @click="handleKnock" style="
-    display: flex;
-    left: 50%;
-    position: relative;
-    top: 20px;
-	transition: width 0.5s ease;" />
+	<img width="100" src="/muyu.svg" alt="我是木鱼" @click="handleKnock" :style="style" />
 </template>
 
 <script setup>
 import { post } from '../utils/api';
 import CryptoJS from 'crypto-js';
 import { useLoadingBar, useMessage } from "naive-ui";
+import { ref, computed } from 'vue';
 const message = useMessage();
 const loadingBar = useLoadingBar();
+const baseStyle = {
+	position: 'relative',
+	top: '50px',
+}
+const animeStyle = {
+	animation: 'run 0.2s linear'
+}
+const isAnimePlaying = ref(false);
+const style = computed(() => isAnimePlaying.value ? { ...baseStyle, ...animeStyle } : baseStyle);
 async function handleKnock() {
+	isAnimePlaying.value = true;
 	post('/api/user/challenge/').then((data) => {
 		const { challenge, difficulty } = data;
 		loadingBar.start();
@@ -39,6 +45,8 @@ async function handleKnock() {
 	).catch((err) => {
 		console.error(err);
 		message.error('木鱼被敲坏了......');
-	})
+	});
+	setTimeout(() => { isAnimePlaying.value = false; }, 200);
+
 }
 </script>
