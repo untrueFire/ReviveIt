170ef6f7d7e3aafe12987dfcb4f8cc5c6bb23fa4
testuser4 <admin.copilot991@passinbox.com>
2024-10-21 19:18:02 +0800

    Fix message badge

--- frontend/src/components/NotificationButton.vue
+++ frontend/src/components/NotificationButton.vue
@@ -1,28 +1,14 @@
 <template>
-    <div>
-        <n-badge :value="store.unreadCount" @click="goToNotifications" style="display: flex; align-items: center;">
-            <n-avatar round>
-                <n-icon>
-                    <NotificationsItem />
-                </n-icon>
-            </n-avatar>
-        </n-badge>
-    </div>
+    <n-badge dot :show="store.unreadCount > 0">
+        <router-link to="/notifications">消息</router-link>
+    </n-badge>
 </template>
 
 <script setup>
 import { onMounted, onUnmounted } from "vue";
-import { useRouter } from "vue-router";
 import { useStore } from '@/store';
 import { updateUnread } from '@/utils/api';
-import { MailOutline as NotificationsItem } from '@vicons/ionicons5';
-const router = useRouter();
 const store = useStore();
-
-const goToNotifications = async () => {
-    await updateUnread();
-    router.push("/notifications");
-};
 
 onMounted(() => {
     store.intervalId = setInterval(updateUnread, 30000);
