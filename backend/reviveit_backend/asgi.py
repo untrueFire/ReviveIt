2a52effc963d6ceb664a5d3c1bc3e1085ffd65d1
 untrueFire <>
2024-10-23 12:09:24 +0800

    Rename project structure
    server -> backend
    client -> frontend

--- backend/reviveit_backend/asgi.py
+++ backend/reviveit_backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for reviveit_server project.
+ASGI config for reviveit_backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reviveit_server.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reviveit_backend.settings")
 
 application = get_asgi_application()
--- backend/reviveit_backend/wsgi.py
+++ backend/reviveit_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for reviveit_server project.
+WSGI config for reviveit_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reviveit_server.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reviveit_backend.settings")
 
 application = get_wsgi_application()
