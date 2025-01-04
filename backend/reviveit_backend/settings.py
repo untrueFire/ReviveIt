9e5f02420b108c6a534b9f0df3629a67fb2c4eb0
untrueFire <untrueFireproton.me@proton.me>
2025-01-04 10:21:51 +0800

    chore: try new database

--- backend/reviveit_backend/settings.py
+++ backend/reviveit_backend/settings.py
@@ -96,8 +96,10 @@ WSGI_APPLICATION = "reviveit_backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "reviveit"
+        },
     }
 }
 
